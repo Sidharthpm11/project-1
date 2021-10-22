eedbc9bdc0ea93e6d56abaa3389c34e6af0bd9e4
Sidharthpm <sidharthpm777@gmail.com>
2021-10-22 20:13:14 +0530

    deploy my react app to githup pages

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+   <div  className="prototype--documentationContainer--DpPNr" >
+    <a class="header--topLeftIcon--3XnCO"><span class=""><svg class="svg" width="12" height="18" viewBox="0 0 12 18" xmlns="http://www.w3.org/2000/svg"><path d="M6 9c0-1.657 1.343-3 3-3 1.657 0 3 1.343 3 3 0 1.657-1.343 3-3 3-1.657 0-3-1.343-3-3z" fill-rule="nonzero" fill-opacity="1" fill="#1abcfe" stroke="none"></path><path d="M0 15c0-1.657 1.343-3 3-3h3v3c0 1.657-1.343 3-3 3-1.657 0-3-1.343-3-3z" fill-rule="nonzero" fill-opacity="1" fill="#0acf83" stroke="none"></path>
+    <path d="M6 0v6h3c1.657 0 3-1.343 3-3 0-1.657-1.343-3-3-3H6z" fill-rule="nonzero" fill-opacity="1" fill="#ff7262" stroke="none"></path><path d="M0 3c0 1.657 1.343 3 3 3h3V0H3C1.343 0 0 1.343 0 3z" fill-rule="nonzero" fill-opacity="1" fill="#f24e1e" stroke="none"></path><path d="M0 9c0 1.657 1.343 3 3 3h3V6H3C1.343 6 0 7.343 0 9z" fill-rule="nonzero" fill-opacity="1" fill="#a259ff" stroke="none"></path></svg></span></a>
+    <a class="header--commentsButton--12w33 action--root--1ClVW header--commentsButton--12w33 action--root--1ClVW" data-tooltip-type="text" data-tooltip="Add comment (C)" data-testid="prototype-header-comments-button"><span class="svg-container"><svg class="svg" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M1.944 16.75l1.696-2.758.287-.465-.237-.493c-.465-.951-.697-1.91-.69-3.034 0-3.866 3.134-7 7-7 3.866 0 7 3.134 7 7 0 3.866-3.134 7-7 7-1.14.007-2.112-.232-3.074-.709l-.268-.131-.296.037-4.418.552zm4.543.44c1.06.518 2.253.81 3.513.81 4.418 0 8-3.582 8-8 0-4.418-3.582-8-8-8-4.418 0-8 3.582-8 8 0 1.243.283 2.419.789 3.468l-2.117 3.44L0 18l1.272-.159 5.215-.652z" fill-rule="nonzero" fill-opacity="1" fill="#000" stroke="none"></path></svg></span></a>
+    <div class="prototype--wrapperFullScale--2bU4j prototype--wrapper--KZ5DX"><div id="viewerContainer" class="prototype--viewer--W7Z-5"><div style="position: absolute; inset: 0px; width: auto; height: auto;"><div style="position: absolute; inset: 0px; width: auto; height: auto;"><canvas width="1033" height="423" style="display: block; left: 0px; top: 0px; width: 826px; height: 338px;"></canvas></div></div></div></div>
+    <h3>SCUS Technology</h3>
+<div className="ww" align="center" >
+
+      <h1>We make products that</h1><h1 style={{color:"blue"}}>matter</h1>
+      <h6>we are more than a agency. We want to be a team within your team,sharing knowledge,and helping one</h6>
+      <input type="button" value="our services"/>
+        <input type="button" value="get a quote"/>
+        <h1> What we do</h1>
+        <p>lorem ipsum dolor sit amet,consectetur adipiscing elit.condimentom id
+          odio iaculis cursus aliquam lectus facilisis laoreer. etiam aliquam nulla
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
+   
+    </div>
+    
+
+  
   );
 }
 
