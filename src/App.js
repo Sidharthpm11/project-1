d348f4e15982c5c6c5cb0db5d4917228a1c0f8ed
Sidharthpm11 <88147140+Sidharthpm11@users.noreply.github.com>
2021-10-22 20:36:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div  className="prototype--documentationContainer--DpPNr" >
+    <a class="header--topLeftIcon--3XnCO"><span class=""><svg class="svg" width="12" height="18" viewBox="0 0 12 18" xmlns="http://www.w3.org/2000/svg"><path d="M6 9c0-1.657 1.343-3 3-3 1.657 0 3 1.343 3 3 0 1.657-1.343 3-3 3-1.657 0-3-1.343-3-3z" fill-rule="nonzero" fill-opacity="1" fill="#1abcfe" stroke="none"></path><path d="M0 15c0-1.657 1.343-3 3-3h3v3c0 1.657-1.343 3-3 3-1.657 0-3-1.343-3-3z" fill-rule="nonzero" fill-opacity="1" fill="#0acf83" stroke="none"></path>
+    <path d="M6 0v6h3c1.657 0 3-1.343 3-3 0-1.657-1.343-3-3-3H6z" fill-rule="nonzero" fill-opacity="1" fill="#ff7262" stroke="none"></path><path d="M0 3c0 1.657 1.343 3 3 3h3V0H3C1.343 0 0 1.343 0 3z" fill-rule="nonzero" fill-opacity="1" fill="#f24e1e" stroke="none"></path><path d="M0 9c0 1.657 1.343 3 3 3h3V6H3C1.343 6 0 7.343 0 9z" fill-rule="nonzero" fill-opacity="1" fill="#a259ff" stroke="none"></path></svg></span></a>
+    <h3>SCUS Technology</h3>
+<div className="ww" align="center" >
+
+      <h1>We make products that</h1><h1 style={{color:"blue"}}>matter</h1>
+      <h6>we are more than a agency. We want to be a team within your team,sharing knowledge,and helping one</h6>
+      <input type="button" value="our services"/>
+        <input type="button" value="get a quote"/>
+        <h1> What we do</h1>
+        <p>lorem ipsum dolor sit amet,consectetur adipiscing elit.condimentom id
+          odio iaculis cursus aliquam lectus facilisis laoreer. etiam aliquam nulla
+        </p>
+    </div>
+   
+    </div>
+    
+
+  
+  );
+}
+
+export default App;
